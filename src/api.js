691e368cefaa582742ecae121ad6f3fe8b41a938
Sodiq-049 <sodiqogunmola049@gmail.com>
2025-07-11 21:25:33 +0100

    frontend updated

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: "https://task-manager-backend-tpl6.onrender.com",
+  baseURL: "https://task-manager-backend-tpl6.onrender.com/api", // ✅ include /api
+  withCredentials: true,
 });
 
 export default API;
